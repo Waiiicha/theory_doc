fe49d35699cb0787c4943b6650b9f5530d8707c2
Waiiicha <Waiiicha@users.noreply.github.com>
2025-04-07 15:07:45 +0000

    Deployed c1d699f with MkDocs version: 1.5.3

---
+++ javascripts/katex-init.js
@@ -0,0 +1,11 @@
+// KaTeX自动渲染配置
+document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(document.body, {
+        delimiters: [
+            {left: '$$', right: '$$', display: true},
+            {left: '$', right: '$', display: false},
+            {left: '\\(', right: '\\)', display: false},
+            {left: '\\[', right: '\\]', display: true}
+        ]
+    });
+});
